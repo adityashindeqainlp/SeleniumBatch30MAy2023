f0b81058da0acd3903141bc8d3107b4a6ceda25f
Aditya Shinde <Aditya Shinde@INFOSEUM33>
2023-08-21 17:41:39 -0400

    Trial 1 Hello World

---
+++ CoreSelenium30May2023/src/Trial/HelloWorld.java
@@ -0,0 +1,14 @@
+package Trial;
+
+/**
+ * @Author -- Aditya Shinde Java + Selenium 21-Aug-2023 5:39:49 pm
+ **/
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello World");
+	}
+
+}
