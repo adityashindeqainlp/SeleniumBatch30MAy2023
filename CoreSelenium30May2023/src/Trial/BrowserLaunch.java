7f37f54f99232cbe077a49d0bb6e2d8a46d5c2b0
Aditya Shinde <Aditya Shinde@INFOSEUM33>
2023-08-24 09:04:21 -0400

    Browser Launch Code with Maven pom.xml Dependency Configuration

---
+++ CoreSelenium30May2023/src/Trial/BrowserLaunch.java
@@ -0,0 +1,37 @@
+package Trial;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+/**
+ * @Author -- Aditya Shinde Java + Selenium 23-Aug-2023 5:19:31 pm
+ **/
+public class BrowserLaunch {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+//		System.setProperty("webdriver.chrome.driver",
+//				"D:\\Aditya_In_lp\\Java\\Setup For Java and Ecllipse\\Drivers\\chromedriver_win32\\chromedriver.exe");
+
+	//	ChromeDriver driver = new ChromeDriver();
+		
+		
+		//WebDriverManager
+		
+		WebDriverManager.chromedriver().setup();
+		
+		
+		//******
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+		
+		
+	}
+
+}
