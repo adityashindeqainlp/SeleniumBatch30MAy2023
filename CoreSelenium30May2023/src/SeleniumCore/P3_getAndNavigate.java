b1d1e7c830c0c3d64585fe1e6ac7aa8bbb7bdd2c
Aditya Shinde <Aditya Shinde@INFOSEUM33>
2023-08-25 09:22:44 -0400

    Program 3  get vs Navigate

---
+++ CoreSelenium30May2023/src/SeleniumCore/P3_getAndNavigate.java
@@ -0,0 +1,43 @@
+package SeleniumCore;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+/**
+ * @Author -- Aditya Shinde Java + Selenium 24-Aug-2023 5:15:43 pm
+ **/
+public class P3_getAndNavigate {
+
+	public static WebDriver driver;
+
+	public static void main(String[] args) throws InterruptedException {
+
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+
+		driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+		Thread.sleep(2000);
+
+		driver.navigate().to("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+
+		Thread.sleep(2000);
+
+		driver.navigate().to("https://mbasic.facebook.com/?_rdr");
+
+		Thread.sleep(2000);
+
+		driver.navigate().back();
+
+		Thread.sleep(2000);
+
+		driver.navigate().forward();
+
+		Thread.sleep(2000);
+
+		driver.navigate().refresh();
+	}
+
+}
