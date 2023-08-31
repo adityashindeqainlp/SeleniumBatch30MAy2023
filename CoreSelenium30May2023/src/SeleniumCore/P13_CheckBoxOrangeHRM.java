7920a87ec565959c155c1560173b1ba5b1707769
Aditya Shinde <Aditya Shinde@INFOSEUM33>
2023-08-31 17:52:42 -0400

    Check OrangeHrm

---
+++ CoreSelenium30May2023/src/SeleniumCore/P13_CheckBoxOrangeHRM.java
@@ -0,0 +1,47 @@
+package SeleniumCore;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+/**
+ * @Author -- Aditya Shinde Java + Selenium 31-Aug-2023 5:08:14 pm
+ **/
+public class P13_CheckBoxOrangeHRM {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+
+		driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+
+		driver.findElement(By.xpath("//input[@placeholder='Username']")).sendKeys("Admin");
+		driver.findElement(By.xpath("//input[@placeholder='Password']")).sendKeys("admin123");
+		driver.findElement(By.xpath("//button[normalize-space()='Login']")).click();
+
+		driver.findElement(By.xpath("(//*[@class='oxd-text oxd-text--span oxd-main-menu-item--name'])[1]")).click();
+
+		List<WebElement> checkBoxes = driver.findElements(
+				By.cssSelector("[class='orangehrm-container'] [class='oxd-icon bi-check oxd-checkbox-input-icon']"));
+
+		for (int i = 1; i < checkBoxes.size(); i++) {
+
+			checkBoxes.get(i).click();
+
+		}
+
+		Thread.sleep(5000);
+
+		driver.quit();
+
+	}
+
+}
