ebba46b91f309737752eb0e87f309e25f23accd7
Aditya Shinde <Aditya Shinde@INFOSEUM33>
2023-09-08 09:18:54 -0400

    Mouse action & Iframe Handling

---
+++ CoreSelenium30May2023/src/SeleniumCore/P26_MouseActionsWithIframe.java
@@ -0,0 +1,33 @@
+package SeleniumCore;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+/**
+ * @Author -- Aditya Shinde Java + Selenium 07-Sep-2023 5:48:40 pm
+ **/
+public class P26_MouseActionsWithIframe {
+
+	public static WebDriver driver;
+
+	public static void main(String[] args) {
+
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://jqueryui.com/slider/");
+
+		WebElement handleOfSlider = driver.findElement(By.xpath("//*[@id='slider']//span"));
+
+		Actions action = new Actions(driver);
+
+		action.clickAndHold().moveToElement(handleOfSlider, 500, 0).build().perform();
+
+	}
+
+}
